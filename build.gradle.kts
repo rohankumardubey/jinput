709d2fa32f2fca56e5f9e90f0f93e1f904096b9c
Duncan Calvert <dcalvert@ihmc.us>
2020-12-03 17:18:49 -0600

    :arrow_up: ihmc-build 0.23.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,12 @@
 plugins {
-   id("us.ihmc.ihmc-build") version "0.23.0"
+   id("us.ihmc.ihmc-build")
    id("us.ihmc.ihmc-ci") version "7.4"
    id("us.ihmc.ihmc-cd") version "1.17"
 }
 
 ihmc {
    group = "us.ihmc.thirdparty.jinput"
-   version = "201202"
+   version = "201202-1"
    vcsUrl = "https://github.com/ihmcrobotics/jinput"
    openSource = true
 
@@ -22,30 +22,10 @@ ihmc {
 }
 
 mainDependencies {
-	api("net.java.jutils:jutils:1.0.0")
+   api("net.java.jutils:jutils:1.0.0")
 	api("org.apache.commons:commons-lang3:3.11")
-	api("us.ihmc:ihmc-native-library-loader:1.3.1")
+   api("us.ihmc:ihmc-native-library-loader:1.3.1")
 	api("net.java.jinput:jinput-platform:2.0.6:natives-windows")
-	api("net.java.jinput:jinput-platform:2.0.6:natives-osx")
+   api("net.java.jinput:jinput-platform:2.0.6:natives-osx")
 	api("net.java.jinput:jinput-platform:2.0.6:natives-linux")
 }
-
-//test.finalizedBy("addPhonyTestXml")
-//
-//task addPhonyTestXml {
-//   doLast {
-//      def testDir = file("$buildDir/test-results/test")
-//      project.mkdir(testDir)
-//
-//      def noTestsFoundFile = file("$testDir/TEST-us.ihmc.NoTestsFoundTest.xml")
-//      noTestsFoundFile.text =
-//            "<?xml version=\"1.0\" encoding=\"UTF-8\"?>" +
-//                  "<testsuite name=\"us.ihmc.NoTestsFoundTest\" tests=\"1\" skipped=\"0\" failures=\"0\" " +
-//                  "errors=\"0\" timestamp=\"2018-10-19T15:10:58\" hostname=\"duncan-ihmc\" time=\"0.01\">" +
-//                  "<properties/>" +
-//                  "<testcase name=\"noTestsFoundTest\" classname=\"us.ihmc.NoTestsFoundTest\" time=\"0.01\"/>" +
-//                  "<system-out>This is a phony test to make CI builds pass when a project does not contain any tests.</system-out>" +
-//                  "<system-err><![CDATA[]]></system-err>" +
-//                  "</testsuite>"
-//   }
-//}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,24 @@
-rootProject.name = "jinput"
+pluginManagement {
+   repositories {
+      mavenLocal()
+      gradlePluginPortal()
+   }
+   plugins {
+      id("us.ihmc.ihmc-build") version "0.23.1"
+   }
+}
+
+buildscript {
+   repositories {
+      maven { url = uri("https://plugins.gradle.org/m2/") }
+      mavenLocal()
+   }
+   dependencies {
+      classpath("us.ihmc:ihmc-build:0.23.1")
+   }
+}
+
+val ihmcSettingsConfigurator = us.ihmc.build.IHMCSettingsConfigurator(settings, logger, extra)
+ihmcSettingsConfigurator.checkRequiredPropertiesAreSet()
+ihmcSettingsConfigurator.configureExtraSourceSets()
+ihmcSettingsConfigurator.findAndIncludeCompositeBuilds()
