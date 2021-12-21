bc02e3bb3720014ab379ff09cb354c2f0251e61f
Duncan Calvert <dcalvert@ihmc.us>
2021-12-20 20:34:55 -0600

    :arrow_up: commons-lang3 3.12.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,7 +21,7 @@ ihmc {
 
 mainDependencies {
    api("net.java.jutils:jutils:1.0.0")
-   api("org.apache.commons:commons-lang3:3.11")
+   api("org.apache.commons:commons-lang3:3.12.0")
    api("us.ihmc:ihmc-native-library-loader:1.3.1")
    api("net.java.jinput:jinput-platform:2.0.6:natives-windows")
    api("net.java.jinput:jinput-platform:2.0.6:natives-osx")
