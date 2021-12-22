a21d4e0476f7272b9ce4e039d438a3cf5abae6d2
Duncan Calvert <dcalvert@ihmc.us>
2021-12-22 15:34:46 -0600

    :arrow_up: ihmc-build 0.27.3 Fix open-snapshots URL

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -4,7 +4,7 @@ pluginManagement {
       gradlePluginPortal()
    }
    plugins {
-      id("us.ihmc.ihmc-build") version "0.27.2"
+      id("us.ihmc.ihmc-build") version "0.27.3"
    }
 }
 
@@ -14,7 +14,7 @@ buildscript {
       mavenLocal()
    }
    dependencies {
-      classpath("us.ihmc:ihmc-build:0.27.2")
+      classpath("us.ihmc:ihmc-build:0.27.3")
    }
 }
 
